06d9d18a662e317c398d037573aeaaaabe5963d8
Akshat Raj <akshatpiyush07@gmail.com>
2026-04-09 22:39:12 +0530

    Get and Post HTTP

--- src/app.js
+++ src/app.js
@@ -2,9 +2,20 @@ const express = require("express");
 
 const app = express();
 
+//This will only handle GET call to to /test
+app.get("/test",(req, res) => {
+    res.send({firstname: "Akshat", lastname: "Piyush"});
+});
+
+app.post("/test",(req, res) => {
+    res.send("Data saved successfull to the database.");
+});
+
+//This will match all the HTTP method API calls to /test
 app.use("/test",(req, res) => {
     res.send("Hello check, hello");
 });
+
 
 app.use((req, res) => {
     res.send("Hello from the server");
